70e289488e25d5fb11bbb5a5723948261315ad58
AnemoneTK <Copp.teenkung@gmail.com>
2024-02-10 13:57:31 +0700

    [IMPORT]-import project file

--- TenAnswers/src/App.jsx
+++ TenAnswers/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import TopNav from "./components/TopNav/TopNav";
+import Button from "./components/Button/Button";
+import Logo from "./components/Logo/Logo";
+import ModeBtn from "./components/ModePopup/ModeBtn";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [popup, setPopup] = useState(false);
+  let popRef = useRef();
+  useEffect(() => {
+    let overlay = (e) => {
+      if (!popRef.current.contains(e.target)) {
+        setPopup(false);
+      }
+    };
+    document.addEventListener("mousedown", overlay);
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="warper">
+        <div
+          className={`overlay ${popup ? "d-block" : "d-none"} 
+          bg-black bg-opacity-50 z-2 w-100 h-100 position-absolute`}
+        ></div>
+        <div className="frame position-relative container col-lg-4 col-md-8 col-sm-12 ">
+          <TopNav/>
+
+          <Logo />
+          <div className="row justify-content-center h-30">
+            <div className="row col-8 m-3">
+              <Button textValue="เล่นต่อ" type="Blue" />
+            </div>
+
+            <div
+              className="row col-8"
+              onClick={() => {
+                setPopup(!popup);
+              }}
+            >
+              <Button textValue="เริ่มเกมใหม่" type="White" />
+            </div>
+          </div>
+
+          <div
+            className={`popup ${popup ? "open" : "close"} 
+            col-10 position-absolute bottom-0 start-50 translate-middle-x 
+            rounded-5 bg-white d-flex flex-column justify-content-center align-items-center z-3`}
+            ref={popRef}
+          >
+            <ModeBtn mode="Easy" />
+            <ModeBtn mode="Normal" />
+            <ModeBtn mode="Hard" />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ TenAnswers/src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import "./Button.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+function Button(props) {
+  return (
+    <>
+        <button className={`${props.type} text-center fs-3 rounded-pill fw-bold col-12 py-2`}>
+          {props.textValue}
+        </button>
+    </>
+  );
+}
+
+export default Button
