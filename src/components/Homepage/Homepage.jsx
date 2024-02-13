89a5e0f35eae2cd8324a612e4be2ad3f1b7021e7
AnemoneTK <Copp.teenkung@gmail.com>
2024-02-13 22:16:04 +0700

    [ADD]-add overlay when click start button

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,52 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import Logo from "../Logo/Logo";
 import Button from "../Button/Button";
 import ModeBtn from "../ModeBtn/ModeBtn";
-
 import TopNav from "../TopNav/TopNav";
 
-function Homepage() {
+function Homepage(props) {
   const [popup, setPopup] = useState(false);
-  // let popRef = useRef();
-  // useEffect(() => {
-  //   let overlay = (e) => {
-  //     if (!popRef.current.contains(e.target)) {
-  //       setPopup(false);
-  //     }
-  //   };
-  //   document.addEventListener("mousedown", overlay);
-  // });
+  let popRef = useRef();
+  useEffect(() => {
+    let overlay = (e) => {
+      if (!popRef.current.contains(e.target)) {
+        setPopup(false);
+        props.open(false);
+      }
+    };
+    document.addEventListener("mousedown", overlay);
+  });
 
   return (
     <>
-      {/* <div
-        className={`overlay ${popup ? "d-block" : "d-none"} 
-          bg-black bg-opacity-50 z-2 position-absolute`} style={{width:"100dvw", height:"100dvh"}}
-      ></div> */}
-      <TopNav back="d-none" hint="d-block"/>
-        <Logo />
-        <div className="row justify-content-center h-30">
-          <div
-            className="row col-8"
-            onClick={() => {
-              setPopup(!popup);
-              // props.open(true)
-            }}
-          >
-            <Button textValue="เริ่มเกมใหม่" type="White" />
-          </div>
-        </div>
-
+      <TopNav back="d-none" hint="d-block" />
+      <Logo />
+      <div className="row justify-content-center h-30">
         <div
-          className={`popup ${popup ? "open" : "close"} 
+          className="row col-8"
+          onClick={() => {
+            setPopup(!popup);
+            props.open(true);
+          }}
+        >
+          <Button textValue="เริ่มเกมใหม่" type="White" />
+        </div>
+      </div>
+
+      <div
+        className={`popup ${popup ? "open" : "close"} 
             col-10 position-absolute bottom-0 start-50 translate-middle-x 
             rounded-5 bg-white d-flex flex-column justify-content-center align-items-center z-3`}
-        >
-          <ModeBtn mode="Easy" />
-          <ModeBtn mode="Normal" />
-          <ModeBtn mode="Hard" />
-        </div>
+        ref={popRef}
+      >
+        <ModeBtn mode="Easy" />
+        <ModeBtn mode="Normal" />
+        <ModeBtn mode="Hard" />
+      </div>
     </>
   );
 }
