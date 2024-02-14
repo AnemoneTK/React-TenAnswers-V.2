bedc6ca7bf491d1789c3e08d2eda2e3544224a63
AnemoneTK <Copp.teenkung@gmail.com>
2024-02-15 00:41:56 +0700

    [FIX]-fix back path 2

--- src/components/ModeBtn/ModeBtn.jsx
+++ src/components/ModeBtn/ModeBtn.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 import "./ModeBtn.css";
 
 function ModeBtn(props) {
+
+  const Mode = props.mode
+
   return (
     <>
       <Link
-        to={`/React-TenAnswers-V.2/game/${props.mode}`}
+        to={`/React-TenAnswers-V.2/game/${Mode}`}
         className={`${props.mode} col-12 d-flex justify-content-center align-items-center`}
       >
         <h1 className="col-10 py-4 text text-center fw-bolder m-0">
