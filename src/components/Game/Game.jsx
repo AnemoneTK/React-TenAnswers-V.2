439337728a564aab174f39b8a12689def81166b5
AnemoneTK <Copp.teenkung@gmail.com>
2024-02-15 01:34:59 +0700

    [ADD]-add get params function

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -7,28 +7,30 @@ import "./Game.css";
 
 function Game(props) {
   const [mode, setMode] = useState("");
-  const { gameMode } = useParams();
-  const [level, setLevel] = useState("")
+  
 
   const [count, setCount] = useState(0);
   const [randomNum, setRandomNum] = useState(0);
-  const [inputTXT, setInputTXT] = useState("");
-  const inputBox = document.getElementById("num-input");
+  const [inputTXT, setInputTXT] = useState('');
+  const inputBox = document.getElementById('num-input')
   const [warning, setWarning] = useState(false);
-  const [result, setResult] = useState("คุณชนะ");
-  const [popup, setPopup] = useState("close");
-  const [showValue, setShowValue] = useState("");
-
+  const [result,setResult] = useState('คุณชนะ')
+  const [popup, setPopup] = useState('close')
+  const [showValue, setShowValue] = useState('')
+ function getParams(){
+  const { gameMode } = useParams()
+  return gameMode
+ }
   useEffect(() => {
     return () => {
-      setLevel(gameMode)
+      getParams()
       props.open(false);
       setRandomNum(Math.floor(Math.random() * 100));
-      setPopup("close");
-      setInputTXT("");
-      if (level == "easy") {
+      setPopup('close')
+      setInputTXT('')
+      if (gameMode == "easy") {
         setMode("ง่าย");
-      } else if (level == "normal") {
+      } else if (gameMode == "normal") {
         setMode("ปานกลาง");
       } else {
         setMode("ยาก");
@@ -36,56 +38,52 @@ function Game(props) {
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
+  
   function SubmitAnswer() {
     // eslint-disable-next-line no-constant-condition
-    let input = inputBox.value;
+    let input = inputBox.value
     if (input >= 0 && input <= 99 && input % 1 == 0 && input != "") {
-      setWarning(false);
-      CheckWin(input, count);
-      ShowValue(input);
-      setCount(count + 1);
-      if (inputTXT == "") {
-        setInputTXT(input);
-      } else {
-        setInputTXT(inputTXT + " , " + input);
+      setWarning(false)
+      CheckWin(input,count)
+      ShowValue(input)
+      setCount(count + 1)
+      if(inputTXT == ''){
+      setInputTXT(input)
+      }else{
+        setInputTXT(inputTXT + ' , ' + input)
       }
-      inputBox.value = "";
-    } else {
-      setWarning(true);
-      inputBox.value = "";
+      inputBox.value = ''
+    }else{
+      setWarning(true)
+      inputBox.value = ''
     }
-    inputBox.focus();
+    inputBox.focus()
   }
 
-  function CheckWin(input, round) {
-    if (input == randomNum) {
-      setPopup("open");
+  function CheckWin(input,round){
+    if(input == randomNum){
+      setPopup('open')
       props.open(true);
-    } else if (round == 9 && input != randomNum) {
-      setPopup("open");
-      setResult("คุณแพ้");
+    } else if (round == 9 && input != randomNum){
+      setPopup('open')
+      setResult('คุณแพ้')
       props.open(true);
-      inputBox.disabled = true;
+      inputBox.disabled = true
     }
   }
-  function ShowValue(num) {
-    if (num > randomNum) {
-      setShowValue("มีค่าน้อยกว่า " + num);
-    } else if (num < randomNum) {
-      setShowValue("มีค่ามากกว่า " + num);
+  function ShowValue(num){
+    if(num > randomNum){
+      setShowValue('มีค่าน้อยกว่า '+ num)
+    }else if(num < randomNum){
+      setShowValue('มีค่ามากกว่า '+ num)
+
     }
   }
 
   return (
     <>
-      <GamePopup
-        num={randomNum}
-        display={popup}
-        round={count}
-        result={result}
-      />
-      <div className={`${level} row justify-content-center mt-5`}>
+      <GamePopup num={randomNum} display={popup} round={count} result={result}/>
+      <div className={`${gameMode} row justify-content-center mt-5`}>
         {mode}
       </div>
       <div className="row justify-content-center fs-3 mb-5 text-white">
@@ -93,7 +91,7 @@ function Game(props) {
       </div>
       <div
         className={`showInput row d-flex flex-column justify-content-center align-items-center ${
-          level == "easy" ? "visible" : "invisible"
+          gameMode == "easy" ? "visible" : "invisible"
         }`}
       >
         <div className="row col-12 justify-content-center text-white fs-5">
@@ -105,7 +103,7 @@ function Game(props) {
       </div>
       <div
         className={`row my-5 ${
-          level == "easy" || level == "normal" ? "visible" : "invisible"
+          gameMode == "easy" || gameMode == "normal" ? "visible" : "invisible"
         }`}
       >
         <div className="showValue col-12 fs-1 bg-white d-flex justify-content-center align-items-center">
@@ -131,23 +129,21 @@ function Game(props) {
               id="num-input"
               className="num-input row col-lg-4 col-md-8 col-sm-12 w-100 text-center mb-5"
               placeholder="กรุณากรอกตัวเลข"
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  SubmitAnswer();
-                }
-              }}
+              onKeyDown={(e) => { 
+                if (e.key === "Enter") { 
+                  SubmitAnswer()
+                } 
+            }} 
             />
           </div>
-          <button
-            className="White text-center fs-3 rounded-pill fw-bold col-12 py-2"
-            onClick={SubmitAnswer}
-          >
+          <button className="White text-center fs-3 rounded-pill fw-bold col-12 py-2" onClick={SubmitAnswer}>
             ยืนยัน
           </button>
         </div>
       </div>
     </>
   );
+
 }
 
 export default Game;
