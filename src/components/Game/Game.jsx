25b977ab2f50780f21cbbfa6ffdfcf2f7b85774d
AnemoneTK <Copp.teenkung@gmail.com>
2024-02-15 00:49:47 +0700

    [FIX]-fix get gammode

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -7,7 +7,8 @@ import "./Game.css";
 
 function Game(props) {
   const [mode, setMode] = useState("");
-  let { gameMode } = useParams();
+  const [gameMode, setGameMode] = useState('')
+  setGameMode(useParams())
 
   const [count, setCount] = useState(0);
   const [randomNum, setRandomNum] = useState(0);
