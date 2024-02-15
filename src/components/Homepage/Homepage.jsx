26822980108cfe17af7a5becd773230af0def5ef
AnemoneTK <Copp.teenkung@gmail.com>
2024-02-16 01:23:12 +0700

    [ADD]-add popref

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -8,19 +8,17 @@ import TopNav from "../TopNav/TopNav";
 
 function Homepage() {
   const [popup, setPopup] = useState(false);
-  // const popRef = useRef();
+  const popRef = useRef();
 
-  // useEffect(() => {
-  //   setPopup(false);
-  //   const screen = (e) => {
-  //     if (!popRef.current.contains(e.target)) {
-  //       setPopup(false);
-  //       props.open(false);
-  //     }
-  //   };
-  //   document.addEventListener("mousedown", screen);
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+  useEffect(() => {
+    const screen = (e) => {
+      if (!popRef.current.contains(e.target)) {
+        setPopup(false);
+      }
+    };
+    document.addEventListener("mousedown", screen);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
@@ -42,7 +40,7 @@ function Homepage() {
         className={`popup ${popup ? "open" : "close"} 
             col-10 position-absolute bottom-0 start-50 translate-middle-x 
             rounded-5 bg-white d-flex flex-column justify-content-center align-items-center z-3`}
-        // ref={popRef}
+        ref={popRef}
       >
         <div to="/game" className="easy row col-12 d-flex justify-content-center align-items-center" onClick={()=> sessionStorage.setItem("level", "Easy")}>
           <ModeBtn text="Easy" mode="easy" />
