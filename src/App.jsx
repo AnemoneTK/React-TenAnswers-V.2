c78aec1640ffede2c875a8e5db9496a491ddc6af
AnemoneTK <Copp.teenkung@gmail.com>
2024-02-15 10:39:11 +0700

    [PULL]-pull back up file

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
 import Homepage from "./components/Homepage/Homepage";
 import Tutorial from "./components/Tutorial/Tutorial";
 import Game from "./components/Game/Game";
@@ -13,11 +11,11 @@ import Overlay from "./components/Overlay";
 
 function App() {
   const [openOverlay, setOverlay] = useState(false);
+  // const [mode, setMode] = useState("");
 
   return (
     <>
-      <Overlay open={openOverlay} />
-
+      <Overlay open={openOverlay}/>
       <div className="warper">
         <div className="frame position-relative container col-lg-4 col-md-8 col-sm-12 ">
           <Switch>
@@ -31,17 +29,6 @@ function App() {
               <Game open={(openOverlay) => setOverlay(openOverlay)} />
             </Route>
           </Switch>
-          {/* <Switch>
-            <Route path="/React-TenAnswers-V.2/" exact>
-              <Homepage open={(openOverlay) => setOverlay(openOverlay)} />
-            </Route>
-            <Route path="/React-TenAnswers-V.2/tutorial">
-              <Tutorial />
-            </Route>
-            <Route path="/React-TenAnswers-V.2/game?level=:gameMode">
-              <Game open={(openOverlay) => setOverlay(openOverlay)} />
-            </Route>
-          </Switch> */}
         </div>
       </div>
     </>
--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,21 +1,17 @@
-import { Link } from "react-router-dom";
+import {
+  Link,
+} from "react-router-dom";
 
 function TopNav(props) {
   return (
     <>
       <div className="nav row p-3 align-items-center h-10">
-        <Link
-          to="/React-TenAnswers-V.2/"
-          className={`${props.back}  col text-start`}
-        >
-          <i className="fa-solid fa-arrow-left fs-4 text-white"></i>
-        </Link>
-        <Link
-          to="/React-TenAnswers-V.2/tutorial"
-          className={`${props.hint}  col text-end`}
-        >
-          <i className="fa-regular fa-circle-question fs-2 text-white"></i>
-        </Link>
+          <Link to="/" className={`${props.back}  col text-start`}>
+            <i className="fa-solid fa-arrow-left fs-4 text-white"></i>
+          </Link>
+          <Link to="/tutorial" className={`${props.hint}  col text-end`}>
+            <i className="fa-regular fa-circle-question fs-2 text-white"></i>
+          </Link>
       </div>
     </>
   );
